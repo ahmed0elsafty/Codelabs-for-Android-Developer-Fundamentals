f858b08af9862e455ff618034cb62094aa7aa1da
El-Safty <afawzy629@gmail.com>
2020-07-19 07:28:40 +0200

    Add some input controls such as EditTexts and RadioButtons

--- DroidCafe/app/src/main/java/com/elsafty/droidcafe/OrderActivity.java
+++ DroidCafe/app/src/main/java/com/elsafty/droidcafe/OrderActivity.java
@@ -1,12 +1,16 @@
 package com.elsafty.droidcafe;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
     private TextView mRecieveMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,32 @@ public class OrderActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.MESSAGE);
         mRecieveMessage.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.same_day:
+                if (checked){
+                    displayMessage(getString(R.string.same_day_messenger_service));
+                }
+                break;
+            case R.id.next_day:
+                if (checked){
+                    displayMessage(getString(R.string.next_day_ground_delivery));
+                }
+                break;
+            case R.id.pick_up:
+                if (checked){
+                    displayMessage(getString(R.string.pick_up));
+                }
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private void displayMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
