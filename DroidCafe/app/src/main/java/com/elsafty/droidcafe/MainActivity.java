89410834746d8a883c059a53a948a876af6ed1bc
El-Safty <afawzy629@gmail.com>
2020-07-19 17:13:20 +0200

    Add Context Menu for select one item and popup menu for it

--- DroidCafe/app/src/main/java/com/elsafty/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/com/elsafty/droidcafe/MainActivity.java
@@ -2,17 +2,22 @@ package com.elsafty.droidcafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String MESSAGE = "message";
     private String mSendMessage;
+
+    private TextView donutTextView,iceCreamTextView,froyoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        donutTextView = findViewById(R.id.donut_description);
+        iceCreamTextView= findViewById(R.id.iceCream_description);
+        froyoTextView = findViewById(R.id.froyo_description);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +39,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(orderIntent);
             }
         });
+        registerForContextMenu(donutTextView);
+        registerForContextMenu(iceCreamTextView);
+        registerForContextMenu(froyoTextView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterForContextMenu(donutTextView);
+        unregisterForContextMenu(iceCreamTextView);
+        unregisterForContextMenu(froyoTextView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_share:
+                displayToast(getString(R.string.action_share));
+                return true;
+            case R.id.action_edit:
+                displayToast(getString(R.string.action_edit));
+                return true;
+            case R.id.action_delete:
+                displayToast(getString(R.string.action_delete));
+                return true;
+        }
+        return super.onContextItemSelected(item);
     }
 
     @Override
